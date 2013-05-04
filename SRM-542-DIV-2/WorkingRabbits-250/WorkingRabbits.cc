e454abc007ef05cb29954a35ae9458bbc3937783
Moda <moda.hsu@gmail.com>
2013-05-04 17:02:29 +0800

    SRM-542-DIV-2:250

---
+++ SRM-542-DIV-2/WorkingRabbits-250/WorkingRabbits.cc
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define eps (le-7)
+#define INF (le60)
+#define MAX (1<<30)
+#define MIN (-MAX)
+#define LL long long
+
+#define pb push_back
+
+#define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
+#define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define ISEQ(c) (c).begin(), (c).end()
+#define gcd(a,b) __gcd(a,b)
+#define EQ(a,b) (abs(a-b) <eps)
+#define sqr(a) ((a)*(a))
+#define popcount(n) __builtin_popcount(n)
+
+inline LL strtoint(const string &s) {stringstream ss;ss<<s;LL ret;ss>>ret;return ret;}
+inline string inttostr(const LL &a) {stringstream ss;ss<<a;string ret;ss>>ret;return ret;}
+
+class WorkingRabbits {
+
+  public: double getEfficiency(vector <string> profit) {
+             int row = profit.size();
+             int col = profit[0].length();
+             double sum=0.0;
+             for(int i=0;i<row;i++)
+             {
+                for(int j=i+1;j<col;j++)
+                {
+                   sum+=profit[i][j]-'0';
+                }
+             }
+             double N=(row-1)*row/2;
+		return sum/N;
+	}
+
+};
