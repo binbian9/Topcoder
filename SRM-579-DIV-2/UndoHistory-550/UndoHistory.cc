17a07c0f73cefb71d891369dc2b6641d1cc6e63d
Moda <moda.hsu@gmail.com>
2013-05-23 00:31:37 +0800

    SRM-579-DIV-2:500

--- SRM-579-DIV-2/UndoHistory-550/UndoHistory.cc
+++ SRM-579-DIV-2/UndoHistory-550/UndoHistory.cc
@@ -23,14 +23,52 @@ using namespace std;
 #define EQ(a,b) (abs(a-b) <eps)
 #define sqr(a) ((a)*(a))
 #define popcount(n) __builtin_popcount(n)
-
+#define sz(x) x.size()
 inline LL strtoint(const string &s) {stringstream ss;ss<<s;LL ret;ss>>ret;return ret;}
 inline string inttostr(const LL &a) {stringstream ss;ss<<a;string ret;ss>>ret;return ret;}
 
 class UndoHistory {
 
-  public: int minPresses(vector <string> lines) {
-		return $DUMMYRETURN$;
+  public: 
+     int minPresses(vector <string> lines) {
+             int size = lines.size();
+
+             int res = lines[0].length()+1;
+             
+             cout<<"START res="<<res<<endl;
+             for(int i=1;i<size;i++)
+             {
+                int mx=0,my=0;
+                for(int j=0;j<i;j++)
+                {
+                   int a=0,b=0;
+                   while(a<sz(lines[i]) && b<sz(lines[j]) && lines[i][a]==lines[j][b])
+                      a++,b++;
+                   mx = max(a,mx);
+                }
+
+                string x = lines[i];
+                string y = lines[i-1];
+                int a=0,b=0;
+                while(a<sz(x) && b<sz(y) && x[a]==y[b])
+                   a++,b++;
+                if(b==sz(y))
+                   my=b;
+                else
+                   my=-10;
+                cout<<"mx="<<mx<<", my="<<my<<endl;
+
+                if(mx-2>my)
+                   res+=sz(lines[i])-mx+2;
+                else
+                   res+=sz(lines[i])-my;
+
+                res++;
+
+
+               cout<<"res="<<res<<endl; 
+             }
+		return res;
 	}
 
 };
