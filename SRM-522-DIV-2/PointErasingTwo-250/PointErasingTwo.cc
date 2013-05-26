380013d4d8fc191df09efb5bdab9857be7e29d4a
Moda_hsu <moda.hsu@gmail.com>
2013-05-26 09:23:45 +0800

    SRM-522-DIV-2:250

---
+++ SRM-522-DIV-2/PointErasingTwo-250/PointErasingTwo.cc
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define eps (le-7)
+#define INF (le60)
+#define MAX (1<<30)
+#define MIN (-MAX)
+#define LL long long
+
+#define pb push_back
+
+#define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
+#define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define ISEQ(c) (c).begin(), (c).end()
+#define gcd(a,b) __gcd(a,b)
+#define EQ(a,b) (abs(a-b) <eps)
+#define sqr(a) ((a)*(a))
+#define popcount(n) __builtin_popcount(n)
+
+inline LL strtoint(const string &s) {stringstream ss;ss<<s;LL ret;ss>>ret;return ret;}
+inline string inttostr(const LL &a) {stringstream ss;ss<<a;string ret;ss>>ret;return ret;}
+
+class PointErasingTwo {
+
+  public: int getMaximum(vector <int> y) {
+             int size =y.size();
+             int ans=0;
+             for(int i=0;i<size;i++)
+             {
+                for(int j=i+1;j<size;j++)
+                {
+                   int best=0;
+                   for(int k=0;k<size;k++)
+                   {
+                      if(i!=k && j!=k && k>i && k<j)
+                      {
+                         if((y[i]<y[k] && y[k]<y[j] )||(y[j]<y[k] && y[k]<y[i]) )
+                            best++;
+                      }
+                   }
+                   ans=max(best,ans);
+                }
+             }
+		return ans;
+	}
+
+};
