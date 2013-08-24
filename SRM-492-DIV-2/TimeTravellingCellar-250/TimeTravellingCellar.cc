ba075a68c7f21ffcb4e49a3597663b415401538f
Moda <moda.hsu@gmail.com>
2013-08-24 11:34:31 +0800

    SRM-492-DIV-2:250

---
+++ SRM-492-DIV-2/TimeTravellingCellar-250/TimeTravellingCellar.cc
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define eps (le-7)
+#define INF (le60)
+#define MAX (1<<30)
+#define MIN (-MAX)
+#define LL long long
+
+#define pb push_back
+
+#define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
+#define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define ISEQ(c) (c).begin(), (c).end()
+#define gcd(a,b) __gcd(a,b)
+#define EQ(a,b) (abs(a-b) <eps)
+#define sqr(a) ((a)*(a))
+#define popcount(n) __builtin_popcount(n)
+
+inline LL strtoint(const string &s) {stringstream ss;ss<<s;LL ret;ss>>ret;return ret;}
+inline string inttostr(const LL &a) {stringstream ss;ss<<a;string ret;ss>>ret;return ret;}
+
+class TimeTravellingCellar {
+
+  public: int determineProfit(vector <int> profit, vector <int> decay) {
+             int size = profit.size();
+             int pro = 0;
+             for(int i=0;i<size;i++)
+             {
+                for(int j=0;j<size;j++)
+                {
+                   if(i!=j && profit[i]-decay[j]>pro)
+                      pro = profit[i]-decay[j];
+                }
+             }
+		return pro;
+	}
+
+};
