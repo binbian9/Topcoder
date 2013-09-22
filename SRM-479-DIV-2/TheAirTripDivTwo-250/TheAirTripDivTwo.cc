915e2688817a125ed157f51d52a08a85c45030fa
Moda <moda.hsu@gmail.com>
2013-09-22 18:53:08 +0800

    SRM-479-DIV-2:250

---
+++ SRM-479-DIV-2/TheAirTripDivTwo-250/TheAirTripDivTwo.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+#define eps (le-7)
+#define INF (le60)
+#define MAX (1<<30)
+#define MIN (-MAX)
+#define LL long long
+
+#define pb push_back
+
+#define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
+#define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define ISEQ(c) (c).begin(), (c).end()
+#define gcd(a,b) __gcd(a,b)
+#define EQ(a,b) (abs(a-b) <eps)
+#define sqr(a) ((a)*(a))
+#define popcount(n) __builtin_popcount(n)
+
+inline LL strtoint(const string &s) {stringstream ss;ss<<s;LL ret;ss>>ret;return ret;}
+inline string inttostr(const LL &a) {stringstream ss;ss<<a;string ret;ss>>ret;return ret;}
+
+class TheAirTripDivTwo {
+
+  public: int find(vector <int> flights, int fuel) {
+             int sum = 0 ;
+             int size = flights.size();
+             for(int i=0;i<size;i++)
+             {
+                fuel-=flights[i];
+                if(fuel>=0)
+                   sum ++;
+                else
+                   break;
+             }
+		return sum;
+	}
+
+};
