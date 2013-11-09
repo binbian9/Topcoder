c8c2a8fdd1e3387b92f15dea1d3c568e5f29bfbe
Moda <moda.hsu@gmail.com>
2013-11-09 12:29:38 +0800

    SRM-596-DIV-2:250

---
+++ SRM-596-DIV-2/FoxAndSightseeing-250/FoxAndSightseeing.cc
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+#define eps (le-7)
+#define INF (le60)
+#define MAX (1<<30)
+#define MIN (-MAX)
+#define LL long long
+
+#define pb push_back
+
+#define FOR(i,s,e) for (int i = int(s); i != int(e); i++)
+#define FORIT(i,c) for (typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define ISEQ(c) (c).begin(), (c).end()
+#define gcd(a,b) __gcd(a,b)
+#define EQ(a,b) (abs(a-b) <eps)
+#define sqr(a) ((a)*(a))
+#define popcount(n) __builtin_popcount(n)
+
+inline LL strtoint(const string &s) {stringstream ss;ss<<s;LL ret;ss>>ret;return ret;}
+inline string inttostr(const LL &a) {stringstream ss;ss<<a;string ret;ss>>ret;return ret;}
+
+class FoxAndSightseeing {
+
+  public: int getMin(vector <int> position) {
+             int size = position.size();
+             int m = 9999999;
+             int sum = 0;
+             for(int i=0;i<size-1;i++)
+             {
+                sum += abs(position[i]-position[i+1]);
+             }
+             m = sum;
+             for(int i=1;i<size-1;i++)
+             {
+                int temp = abs(position[i-1]-position[i])+abs(position[i]-position[i+1]) - abs(position[i-1]-position[i+1]);
+                if (m > sum-temp)
+                   m = sum-temp;
+             }
+		return m;
+	}
+
+};
